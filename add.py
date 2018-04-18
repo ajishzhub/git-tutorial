00fea2627110da388653241d6a59a1b659338dc6
user <user@fisat>
2018-04-18 05:00:21 -0400

    LAST PROGRAM

--- add.py
+++ add.py
@@ -4,14 +4,19 @@ num2 = input('Enter second number: ')
 # Add two numbers
 sum = float(num1) + float(num2)
 print sum
-def add2(num1, num2):
+def add1(num1, num2):
 			s = num1 + num2
 			return s
 
-		print add2(3, 4)
-def add2(num1, num2):
-			s = num1 - num2
+print add2(3, 4)
+def add2(num1, num2,num3):
+			s = num1 + num2 +num3
 			return s
 
-print add2(3, 4)
+print add2(3, 4, 4 )
+	def add3(num1, num2,num3,num4):
+			s = num1 + num2 +num3 +num4
+			return s
+
+print add2(3,4,4,5)
 	
