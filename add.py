542df5ff1c219f542ca17755f7ff232ab13e6cd7
user <user@fisat>
2018-04-18 04:46:07 -0400

    AJISH ANTONY

---
+++ add.py
@@ -0,0 +1,12 @@
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+print sum
+def add2(num1, num2):
+			s = num1 + num2
+			return s
+
+		print add2(3, 4)
+	
